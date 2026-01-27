8d2792509ff8ec09058eb30152b795f37a169184
lilamendes <lillamendes@hotmail.com>
2026-01-26 22:44:47 -0300

    Atualizações no projeto

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,13 @@
+describe('Login', ()=>{
+  it('Deve logar com sucesso',()=>{
+    cy.viewport(1440,900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana123')
+    cy.contains('button','Entrar').click()
+
+    cy.wait(3000)
+
+  })
+})
