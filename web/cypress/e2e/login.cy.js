43a6e8086f1b63d2f6d471ddac7a68d76005f27b
lilamendes <lillamendes@hotmail.com>
2026-01-28 00:01:27 -0300

    Atualizacao com custom commands e talz

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,13 +1,26 @@
-describe('Login', ()=>{
-  it('Deve logar com sucesso',()=>{
-    cy.viewport(1440,900)
-    cy.visit('http://localhost:3000')
+describe('Login', () => {
+  it('Deve logar com sucesso', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
 
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana123')
-    cy.contains('button','Entrar').click()
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
 
-    cy.wait(3000)
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+  })
 
+  it('Não deve logar com senha inválida', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123errada')
+    cy.contains('Acesso negado! Tente novamente.')
+  })
+
+  it('Não deve logar com email não cadastrado', () => {
+    cy.start()
+    cy.submitLoginForm('papitoErrado@webdojo.com', 'katana123')
+    cy.contains('Acesso negado! Tente novamente.')
   })
 })
